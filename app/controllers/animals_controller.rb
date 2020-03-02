8fc691a5b23d521398a239a0850dcb2b7ceea8fb
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-03-02 17:16:10 +0800

    created animals controller with index, show, new, create actions, renamed type column to species in animals table, created views for index, new, show

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,30 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.user = current_user
+    if @animal.save
+      redirect_to animal_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :category, :species, :hourly_rate, :gender, :user, :place_of_origin, :description, :age)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_074102) do
+ActiveRecord::Schema.define(version: 2020_03_02_090359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
-    t.string "type"
+    t.string "species"
     t.string "name"
     t.integer "age"
     t.text "description"
