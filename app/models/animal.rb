b2b4e53558697933dbc3f68470a86f171ab73a96
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-03-02 16:03:25 +0800

    fixed spelling mistake of amphibians

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,5 @@
 class Animal < ApplicationRecord
-  CATEGORIES = ['Birds', 'Mammals', 'Reptiles', 'Marine', 'Ambhibians', 'Insects', 'Other']
+  CATEGORIES = ['Birds', 'Mammals', 'Reptiles', 'Marine', 'Amphibians', 'Insects', 'Other']
   belongs_to :user
   validates :name, :type, :hourly_rate, :category, :gender, :user, presence: true
   validates :category, inclusion: { in: CATEGORIES }
