364b6184449274066b379252584e5e886c202a33
brandon olin <olin.brandon@gmail.com>
2020-03-03 12:03:52 +0800

    cleaned up validation syntax and added defaults for boolean values in the migration

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :animal
   belongs_to :user
-  validates :date, :start_time, :duration, :total_fee, :animal_id, :user_id, presence: true
+  validates :date, :start_time, :duration, :total_fee, :animal, :user, presence: true
 end
