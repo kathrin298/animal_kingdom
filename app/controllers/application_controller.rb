e5b4ac548d8b383a8b73bbea844ac5fce35d289f
John Kealy <johnckealy@gmail.com>
2020-03-03 10:06:31 +0800

    small patch to devise code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # include pundit
+  # after action :verify... pundit commands here
+
+
+  ## This is to sanitize in case you'd like to add anything else to the
+  ## signup page e.g. first name, etc. (this was added by John)
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # def configure_permitted_parameters
+    ## For additional fields in app/views/devise/registrations/new.html.erb
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    ## For additional in app/views/devise/registrations/edit.html.erb
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  # end
 end
