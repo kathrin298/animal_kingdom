c2d0f01f7e22389d91ba5626ecded7cb70d038f6
Ivan Hossain <ivan2192011@gmail.com>
2020-03-03 17:46:25 +0800

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,4 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
   validates :rating, numericality: { only_integer: true }
-  has_many :review, through :booking
 end
