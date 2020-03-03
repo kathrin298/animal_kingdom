f60094d89f627951f309ba4be068c22bbd6dad25
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-03-03 12:05:56 +0800

    created routes for delete, update and edit and added those actions to the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'animals#index'
-  resources :animals, only: [:index, :new, :create, :show]
+  resources :animals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
