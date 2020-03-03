72903439c15912be3aaec22b742bf753e56247d7
brandon olin <olin.brandon@gmail.com>
2020-03-03 14:38:40 +0800

    added routes for bookings, nested the new and create methods within the animals resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'animals#index'
-  resources :animals
+  resources :animals do
+    resources :bookings, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #TODO resources :booking, only [:] do
   resources :reviews, only: [:create, :new]
-
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
 end
