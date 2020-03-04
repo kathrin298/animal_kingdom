18cfa97fa4671a345680ed8c11d5a1a762cdde14
brandon olin <olin.brandon@gmail.com>
2020-03-04 16:58:54 +0800

    fixed shcema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_055412) do
+ActiveRecord::Schema.define(version: 2020_03_04_041936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,10 +70,10 @@ ActiveRecord::Schema.define(version: 2020_03_04_055412) do
     t.string "title"
     t.text "content"
     t.integer "rating"
+    t.bigint "booking_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "booking_id"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
@@ -98,4 +98,5 @@ ActiveRecord::Schema.define(version: 2020_03_04_055412) do
   add_foreign_key "bookings", "animals"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
