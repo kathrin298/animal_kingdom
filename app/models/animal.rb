7502d61ff40efd4a5eeaf1fcc8e9c81c597f0c71
Ivan Hossain <ivan2192011@gmail.com>
2020-03-04 16:06:46 +0800

    roundup to 1

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,16 +2,16 @@ class Animal < ApplicationRecord
   CATEGORIES = ['Birds', 'Mammals', 'Reptiles', 'Marine', 'Amphibians', 'Insects', 'Other']
   GENDERS = ['Male', 'Female']
   has_many :bookings
-  has_many :reviews, through: :bookings #add-today
+  has_many :reviews, through: :bookings
   belongs_to :user
   has_many_attached :photos
   validates :name, :species, :category, :gender, :user, presence: true
   validates :category, inclusion: { in: CATEGORIES }
   validates :gender, inclusion: { in: GENDERS }
 
-  def average_rating  #add-today
+  def average_rating
     if !reviews.empty?
-      reviews.sum(:rating) / reviews.size
+      (reviews.sum(:rating).fdiv reviews.size).round(1)
     else
       "no reviews yet"
     end
