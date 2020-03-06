0824306372967ac27310e0f6ff39a3b98173058d
brandon olin <olin.brandon@gmail.com>
2020-03-06 15:35:45 +0800

    fixed logic, owner bookings show up correctly now

--- app/controllers/accounts/bookings_controller.rb
+++ app/controllers/accounts/bookings_controller.rb
@@ -4,9 +4,10 @@ class Accounts::BookingsController < ApplicationController
   def index
     @user = current_user
     @animal = Animal.new
+    @bookings = Booking.all
     @past_bookings = @user.bookings.select{ |booking| booking.end_date < Date.today }
     @future_bookings = @user.bookings.select{ |booking| booking.end_date > Date.today }
-    @owner_bookings = @user.bookings.select{ |booking| booking.animal.user == current_user }
+    @owner_bookings = @bookings.select{ |booking| booking.animal.user == current_user }
     @animals = @user.animals
   end
 end
